65c9bff47da73b1463c0af1b1bb9458a82ba40ea
Thomas Ngo <tngo0508@gmai.com>
2020-05-27 16:45:09 -0700

    arrow functions

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,89 +1,20 @@
 "use strict";
 
-console.log("App.js is running!");
-
-var app = {
-  title: "Indecision App",
-  subtitle: "Put your life in the hands of a computer",
-  options: ["One", "Two"]
+var square = function square(x) {
+  return x * x;
 };
 
-// JSX - Javascript XML
-var template = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
-    null,
-    app.title
-  ),
-  app.subtitle && React.createElement(
-    "p",
-    null,
-    app.subtitle
-  ),
-  app.options && app.options.length > 0 ? "here are your options" : "No options",
-  React.createElement(
-    "ol",
-    null,
-    React.createElement(
-      "li",
-      null,
-      "Item one"
-    ),
-    React.createElement(
-      "li",
-      null,
-      "Item two"
-    )
-  )
-);
+console.log(square(8));
 
-var user = {
-  name: "Andrew",
-  age: 17,
-  location: "New York"
+// const squareArrow = (x) => {
+//   return x * x;
+// }
+
+var squareArrow = function squareArrow(x) {
+  return x * x;
 };
+console.log(squareArrow(4));
 
-function getLocation(location) {
-  if (location) {
-    return React.createElement(
-      "p",
-      null,
-      "Location: ",
-      location
-    );
-  }
-}
-
-var templateTwo = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
-    null,
-    user.name ? user.name : "Anonamous"
-  ),
-  user.age && user.age >= 18 && React.createElement(
-    "p",
-    null,
-    "Age: ",
-    user.age
-  ),
-  React.createElement(
-    "p",
-    null,
-    "Age: ",
-    user.age
-  ),
-  React.createElement(
-    "p",
-    null,
-    "Location: ",
-    user.location
-  )
-);
-
-var appRoot = document.getElementById("app");
-
-ReactDOM.render(template, appRoot);
+var getFirstName = function getFirstName(name) {
+  return console.log(name.split(" ")[0]);
+};
