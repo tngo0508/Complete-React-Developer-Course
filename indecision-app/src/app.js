8427fda1c87ddd66fa3d55c5896addc1661ea184
Thomas Ngo <tngo0508@gmai.com>
2020-05-28 15:25:17 -0700

    importing npm modules

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,12 +1,5 @@
-// import "./utils.js";
-// import subtract, { square, add } from "./utils";
+import React from "react";
+import ReactDOM from "react-dom";
 
-// console.log("app.js is running!!");
-// console.log(square(33));
-// console.log(add(33, 77));
-// console.log(subtract(100, 81));
-
-import isSenior, { isAdult, canDrink } from "./person";
-console.log(isAdult(18));
-console.log(canDrink(18));
-console.log(isSenior(65));
+const template = React.createElement("p", {}, "testing 123");
+ReactDOM.render(template, document.getElementById("app"));
