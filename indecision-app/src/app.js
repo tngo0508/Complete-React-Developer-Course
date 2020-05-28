0e4941b1e376fb288d71ce42d9c0bab117ca1e55
Thomas Ngo <tngo0508@gmai.com>
2020-05-28 16:42:44 -0700

    ES6 class properties

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,3 +3,28 @@ import ReactDOM from "react-dom";
 import IndecisionApp from "./components/IndecisionApp";
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+
+class OldSyntax {
+  constructor() {
+    this.name = "mike";
+    this.getGreeting = this.getGreeting.bind(this);
+  }
+  getGreeting() {
+    return `Hi. My name is ${this.name}.`;
+  }
+}
+const oldSyntax = new OldSyntax();
+const getGreeting = oldSyntax.getGreeting;
+console.log(getGreeting());
+
+// -----------------
+
+class Newsyntax {
+  name = "Jen";
+  getGreeting = () => {
+    return `Hi. My name is ${this.name}.`;
+  };
+}
+const newSyntax = new Newsyntax();
+const newGetGreeting = newSyntax.getGreeting();
+console.log(newGetGreeting);
