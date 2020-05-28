4074950edc86cf177afcb13ecfa765edd5ee5941
Thomas Ngo <tngo0508@gmai.com>
2020-05-28 15:58:02 -0700

    one component per file

--- indecision-app/src/utils.js
+++
@@ -1,9 +0,0 @@
-console.log("utils.js is running");
-
-export const square = (x) => x * x;
-
-export const add = (a, b) => a + b;
-
-export default (a, b) => a - b;
-// exports - default export - named export
-// export { square, add, substract as default };
