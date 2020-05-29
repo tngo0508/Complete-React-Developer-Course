54a19a11aa0dd0d776e475cb7f9ad1668f4f09e1
Thomas Ngo <tngo0508@gmai.com>
2020-05-29 09:18:14 -0700

    setting up Webpack with scss

--- indecision-app/webpack.config.js
+++ indecision-app/webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
   devtool: "cheap-module-eval-source-map",
