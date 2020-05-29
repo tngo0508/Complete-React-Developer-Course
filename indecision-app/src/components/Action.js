1d701c8619ba7e5f6c6ea6ac8e5f4da016c24a2f
Thomas Ngo <tngo0508@gmai.com>
2020-05-28 23:17:25 -0700

    refractoring other stateless functional components

--- indecision-app/src/components/Action.js
+++ indecision-app/src/components/Action.js
@@ -1,13 +1,12 @@
 import React from "react";
+import Modal from "react-modal";
 
-const Action = (props) => {
-  return (
-    <div>
-      <button onClick={props.handlePick} disabled={!props.hasOptions}>
-        What should I do?
-      </button>
-    </div>
-  );
-};
+const Action = (props) => (
+  <div>
+    <button onClick={props.handlePick} disabled={!props.hasOptions}>
+      What should I do?
+    </button>
+  </div>
+);
 
 export default Action;
