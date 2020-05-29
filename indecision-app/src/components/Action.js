26aca94cb52e45991e973fe679dd330abf0c4c57
Thomas Ngo <tngo0508@gmai.com>
2020-05-29 10:24:46 -0700

    big button & options list

--- indecision-app/src/components/Action.js
+++ indecision-app/src/components/Action.js
@@ -3,7 +3,11 @@ import Modal from "react-modal";
 
 const Action = (props) => (
   <div>
-    <button onClick={props.handlePick} disabled={!props.hasOptions}>
+    <button
+      className="big-button"
+      onClick={props.handlePick}
+      disabled={!props.hasOptions}
+    >
       What should I do?
     </button>
   </div>
