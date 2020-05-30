50ac14e1bf3ebb48cd390445a6687512ede535df
Thomas Ngo <tngo0508@gmai.com>
2020-05-29 23:40:29 -0700

    react-router 101

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,7 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import IndecisionApp from "./components/IndecisionApp";
+import { BrowserRouter, Route } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById("app"));
+const ExpenseDashboardPage = () => (
+  <div>This is from my dashboard component</div>
+);
+
+const AddDashboardPage = () => (
+  <div>This is from my add dashboard component</div>
+);
+const EditDashboardPage = () => (
+  <div>This is from my edit dashboard component</div>
+);
+const HelpPage = () => <div>This is from my help component</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={ExpenseDashboardPage} />
+      <Route exact path="/create" component={AddDashboardPage} />
+      <Route exact path="/edit" component={EditDashboardPage} />
+      <Route exact path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
