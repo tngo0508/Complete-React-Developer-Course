26bb512d1e03cdb945183a64bc8b3600c13ce2db
Thomas Ngo <tngo0508@gmai.com>
2020-05-31 11:08:35 -0700

    es6 array destructuring

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -18,3 +18,12 @@ const {
 console.log(`${name} is ${age}.`);
 
 console.log(`It's ${temp} in ${city}`);
+
+const address = ["123 main street", "westminster", "california", "90876"];
+
+const [street, city_, state, zip] = address;
+
+console.log(`You are in ${street} ${city_} ${state} ${zip}`);
+
+const [, , , zip_] = address;
+console.log(`${zip_}`);
