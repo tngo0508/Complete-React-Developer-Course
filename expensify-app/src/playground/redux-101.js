9adf9c795ff8f971b188eebc8b8de40ed087e4df
Thomas Ngo <tngo0508@gmai.com>
2020-05-31 11:30:28 -0700

    refractoring and organizing

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,18 +1,41 @@
 import { createStore } from "redux";
 
+// Action generators - functions that return action objects
+
+const add = ({ a, b }, c) => {
+  return a + b + c;
+};
+
+console.log(add({ a: 1, b: 12 }, 100));
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: "INCREMENT",
+  incrementBy,
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: "DECREMENT",
+  decrementBy,
+});
+
+const resetCount = () => ({
+  type: "RESET",
+});
+
+const setCount = ({ count }) => ({
+  type: "SET",
+  count,
+});
+
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
-      const incrementBy =
-        typeof action.incrementBy === "number" ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy,
+        count: state.count + action.incrementBy,
       };
     case "DECREMENT":
-      const decrementBy =
-        typeof action.decrementBy === "number" ? action.decrementBy : 1;
       return {
-        count: state.count - decrementBy,
+        count: state.count - action.decrementBy,
       };
     case "RESET":
       return {
@@ -31,31 +54,16 @@ const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({
-  type: "INCREMENT",
-  incrementBy: 5,
-});
+store.dispatch(incrementCount({ incrementBy: 5 }));
 
-store.dispatch({
-  type: "INCREMENT",
-});
+store.dispatch(incrementCount());
 
-store.dispatch({
-  type: "RESET",
-});
+store.dispatch(resetCount());
 
-store.dispatch({
-  type: "DECREMENT",
-});
+store.dispatch(decrementCount());
 
-store.dispatch({
-  type: "DECREMENT",
-  decrementBy: 10,
-});
+store.dispatch(decrementCount({ decrementBy: 10 }));
 
-store.dispatch({
-  type: "SET",
-  count: 101,
-});
+store.dispatch(setCount({ count: -100 }));
 
 unsubscribe();
