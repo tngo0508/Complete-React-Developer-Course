436d29f682ea8896bb985af1c7ca4a58d55aadca
Thomas Ngo <tngo0508@gmai.com>
2020-06-01 11:01:15 -0700

    Controlled inputs for filters

--- expensify-app/src/components/ExpenseListItem.js
+++ expensify-app/src/components/ExpenseListItem.js
@@ -2,12 +2,23 @@
 // description, amount, createdAt
 
 import React from "react";
+import { connect } from "react-redux";
+import { removeExpense } from "../actions/expenses";
 
-export default ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ id, description, amount, createdAt, dispatch }) => (
   <div>
     <h3>{description}</h3>
     <p>
       {amount} - {createdAt}
     </p>
+    <button
+      onClick={(e) => {
+        dispatch(removeExpense({ id }));
+      }}
+    >
+      Remove
+    </button>
   </div>
 );
+
+export default connect()(ExpenseListItem);
